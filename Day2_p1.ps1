7c80b7f5dd377db64be7ccd1f6e83fb92971b5e2
Bungard <mbungar1@kent.edu>
2020-12-02 11:38:54 -0500

    Day2 scripts

---
+++ Day2_p1.ps1
@@ -0,0 +1,29 @@
+﻿$validPws = 0
+foreach($pwLine in Get-Content "G:\My Drive\AdventForCode\2020_Day2_p1.txt")
+{
+    $p = Get-PWParts $pwLine
+    $pwLength = 0
+    for($i=0;$i -lt $p.pw.Length;$i+=1)
+    {
+        if($p.pw[$i] -eq $p.letter)
+        {
+            $pwLength +=1
+        }
+    }
+    #write-host "$pwLength - $pwLine"
+    if($pwLength -ge $p.minCount -and $pwLength -le $p.maxCount)
+    {
+        $validPws +=1
+    }
+}
+
+function Get-PWParts($line)
+{
+    $parts = $line.split(" ")
+    $minCount = $parts[0].split("-")[0]
+    $maxCount = $parts[0].split("-")[1]
+    $letter = $parts[1][0]
+    $pw = $parts[2]
+    return [PSObject]@{"minCount"=$minCount;"maxCount"=$maxCount;"letter"=$letter;"pw"=$pw}
+
+}
---
+++ Day2_p2.ps1
@@ -0,0 +1,19 @@
+﻿$validPws = 0
+foreach($pwLine in Get-Content "G:\My Drive\AdventForCode\2020_Day2_p1.txt")
+{
+    $p = Get-PWParts $pwLine
+    if($p.pw[$p.first-1] -eq $p.letter -xor $p.pw[$p.Second-1] -eq $p.letter)
+    {
+        $validPws +=1
+    }
+}
+$validPws
+function Get-PWParts($line)
+{
+    $parts = $line.split(" ")
+    $First = $parts[0].split("-")[0]
+    $Second = $parts[0].split("-")[1]
+    $letter = $parts[1][0]
+    $pw = $parts[2]
+    return [PSObject]@{"First"=$First;"Second"=$second;"letter"=$letter;"pw"=$pw}
+}
