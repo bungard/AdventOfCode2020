41d130ea32f34f2b792d95131725ff818e445294
Bungard <mbungar1@kent.edu>
2020-12-08 17:14:34 -0500

    Day 3 tasks

---
+++ Day3_p1.ps1
@@ -0,0 +1,27 @@
+﻿$row = 0
+$col = 0
+$treesHit= 0
+$map = Get-Content "G:\My Drive\AdventForCode\2020_Day3.txt"
+while($row -lt $map.Length)
+{
+    if($map[$row][$col] -eq "#" -or $map[$row][$col] -eq "X")
+    { 
+        $treesHit += 1
+        $map[$row].remove($col,1).insert($col,'X')
+    }
+    else {
+        
+        $map[$row].remove($col,1).insert($col,'O')
+    }
+    Write-host $map[$row]
+    $row += 1
+    $col += 3
+
+    
+
+    if($col -ge $map[$row].Length)
+    {
+        $col -= $map[$row].Length
+    }
+}
+$treesHit
---
+++ Day3_p2.ps1
@@ -0,0 +1,30 @@
+﻿function Get-HitTrees([int]$right, [int]$down)
+{
+    $row = 0
+    $col = 0
+    $treesHit= 0
+    $map = Get-Content "G:\My Drive\AdventForCode\2020_Day3.txt"
+    while($row -lt $map.Length)
+    {
+        if($map[$row][$col] -eq "#" )
+        { 
+            $treesHit += 1
+        }
+        $row += $down
+        $col += $right
+
+
+        if($col -ge $map[$row].Length)
+        {
+            $col -= $map[$row].Length
+        }
+    }
+    return $treesHit
+}
+
+$treesHit = Get-HitTrees -Right 1 -down 1
+$treesHit *= Get-HitTrees -Right 3 -down 1
+$treesHit *= Get-HitTrees -Right 5 -down 1
+$treesHit *= Get-HitTrees -Right 7 -down 1
+$treesHit *= Get-HitTrees -Right 1 -down 2
+$treesHit
